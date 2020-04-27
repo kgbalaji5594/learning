6d3813409b9a9034500aa55453dcbacdf2bec335
balaji-fourkiter <balaji@fourkites.com>
2020-04-27 13:17:27 +0530

    test_1:testaa

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -1,12 +1,14 @@
 import unittest
-import arithmetic
+import learning.arithmetic
 
 class SimpleTest(unittest.TestCase):
 
     # Returns True or False.
     def test_add(self):
-        self.assertEqual(arithmetic.add(5, 5), 10)
+        self.assertEqual(learning.arithmetic.add(5, 5), 10)
 
+    def test_multiply(self):
+        self.assertEqual(learning.arithmetic.multiply(5, 5), 25)
 
 if __name__ == '__main__':
     unittest.main()
