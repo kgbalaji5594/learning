d0bdb46412afc6a044ec82d75b53696fe4af3345
kgbalaji5594 <63789883+kgbalaji5594@users.noreply.github.com>
2020-04-28 17:59:11 +0530

    Create test_coverage.sh

---
+++ test_coverage.sh
@@ -0,0 +1,10 @@
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
+pytest --cov=./ --cov-report=xml
+TEST_STATUS=$?
+echo $GIT_COMMIT # only needed for debugging
+GIT_COMMIT=$(git log | grep -m1 -oE '[^ ]+$')
+echo $GIT_COMMIT # only needed for debugging
+./cc-test-reporter after-build -t coverage.py -o ./coverage.xml --exit-code $TEST_STATUS || echo  “Skipping Code Climate coverage upload”
+exit $TEST_STATUS
